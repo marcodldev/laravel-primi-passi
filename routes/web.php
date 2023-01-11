1b78a0001b704915e5281c7603ae942112705d39
marcodldev <marcodelisi2@gmail.com>
2023-01-11 17:12:56 +0100

    + routes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/chisiamo', function () {
+    return view('chisiamo');
+});
+
+Route::get('/startup', function () {
+    return view('startup');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
